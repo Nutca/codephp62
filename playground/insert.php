57c5de47d7491722e9875ee8ef47d429e6a4b8af
Nutcha Sangkane <ffonebsk@gmail.com>
2021-03-29 15:50:15 +0700

    edit insert

--- playground/insert.php
+++ playground/insert.php
@@ -1,14 +1,5 @@
 <?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = "";
-$dbname = 'codephp62';
-
-$myconn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-    if ($myconn->connect_errno) {
-        printf("Connect failed: %s\n", $myconn->connect_error9);
-        exit();
-    }
+require_once 'connectdb.php';
     
     if($_SERVER["REQUEST_METHOD"] == "POST") {
     $frmUsername = $frmPassword = "";
